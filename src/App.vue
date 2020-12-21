efc901bea7e550b7966bf21da02cea8024c1afe7
charlesx19 <39717948+charlesx19@users.noreply.github.com>
2020-12-21 17:28:04 +0800

    add booking state

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,69 @@
   <h1>Create tables</h1>
   <div class="table-count">
     <div class="title">Table Count:</div>
-    <input type="text" v-modal="tableCount">
+    <input type="number" v-model.number="tableCount" />
   </div>
-  <div class="seats">
+  <div v-if="!tableIsSet" class="seats" :class="{ active: tableIsSet }">
     <div class="title">Seats:</div>
     <ul>
-      <li>
-        <div>{{ tableCount }}</div>
-        <input type="text">
+      <li v-for="(n, index) in tableCount" :key="index">
+        <div>{{ n }}/ &nbsp;</div>
+        <input type="number" v-model="seats[n-1]">
       </li>
-      
+    </ul>
+    <button @click="setTable">Create seats</button>
+  </div>
+
+  <div class="table-list" v-if="tableIsSet">
+    <ul>
+      <li v-for="(n, index) in tableCount" :key="index" @dblclick="booking">
+        0/{{ seats[n-1] }}
+      </li>
     </ul>
   </div>
-  <button>Create seats</button>
+
+  <div class="booking-detail">
+    <form action="#">
+      <label for="number">人數</label>
+      <input type="number" id="number">
+      <label for="name">姓名</label>
+      <input type="text" id="name">
+      <label for="phone">電話</label>
+      <input type="text" id="phone">
+      <label for="time">時間</label>
+      <input type="text" id="time">
+    </form>
+    <button @click="bookingDone">Booking</button>
+  </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld
   },
   data() {
     return {
-      tableCount: '4',
-      tableDetial: 
-      [
-        {
-          seats: '',
+      tableCount: 0,
+      tableIsSet: false,
+      seats: [],
+    };
+  },
+  methods: {
+    setTable(){
+      this.tableIsSet = true;
+    },
+    booking(){
 
-        }
-      ]
+    },
+    bookingDone(){
+      
     }
   },
-}
+};
 </script>
 
 <style lang="less">
@@ -69,4 +96,40 @@ h1 {
   margin-bottom: 30px;
 }
 
+.table-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  ul {
+    display: flex;
+    li {
+      padding: 30px;
+      margin: 0 10px;
+      border: 1px solid #000;
+      border-radius: 5px;
+    }
+  }
+}
+
+.booking-detail {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  padding: 50px;
+  border: 1px solid #000;
+  border-radius: 10px;
+  background: #eee;
+  form {
+    display: flex;
+    flex-direction: column;
+    * {
+      text-align: left;
+    }
+  }
+  button {
+    margin-top: 10px;
+  }
+}
 </style>
