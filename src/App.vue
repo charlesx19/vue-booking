394f45b87b603c9f55721f1ad3bd4c899a329bc9
charlesx19 <39717948+charlesx19@users.noreply.github.com>
2020-12-21 15:35:44 +0800

    init vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Create tables</h1>
+  <div class="table-count">
+    <div class="title">Table Count:</div>
+    <input type="text" v-modal="tableCount">
+  </div>
+  <div class="seats">
+    <div class="title">Seats:</div>
+    <ul>
+      <li>
+        <div>{{ tableCount }}</div>
+        <input type="text">
+      </li>
+      
+    </ul>
+  </div>
+  <button>Create seats</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  data() {
+    return {
+      tableCount: '4',
+      tableDetial: 
+      [
+        {
+          seats: '',
+
+        }
+      ]
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style-type: none;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+h1 {
+  margin-bottom: 30px;
+  font-weight: 800;
+}
+
+.title {
+  font-weight: 800;
+}
+
+.table-count {
+  margin-bottom: 30px;
+}
+
 </style>
