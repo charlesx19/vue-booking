a0b37be50e895b85932f6e5f49289c21d04782f1
charlesx19 <39717948+charlesx19@users.noreply.github.com>
2020-12-22 11:45:18 +0800

    + step4: booking modal + CRU feature

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,55 @@
 <template>
   <h1>Create tables</h1>
-  <div class="table-count">
-    <div class="title">Table Count:</div>
-    <input type="number" v-model.number="tableCount" />
-  </div>
-  <div v-if="!tableIsSet" class="seats" :class="{ active: tableIsSet }">
-    <div class="title">Seats:</div>
-    <ul>
-      <li v-for="(n, index) in tableCount" :key="index">
-        <div>{{ n }}/ &nbsp;</div>
-        <input type="number" v-model="seats[n-1]">
-      </li>
-    </ul>
+  <div v-if="!tableIsSet" class="seats">
+    <div class="table-count">
+      <div class="title">Table Count:</div>
+      <input type="number" v-model.number="tableCount" />
+    </div>
+    <div v-if="tableCount" class="title">
+      Seats:
+      <ul>
+        <li v-for="(n, index) in tableCount" :key="index">
+          <div>{{ n }}/ &nbsp;</div>
+          <input type="number" v-model="seats[n-1]">
+        </li>
+      </ul>
     <button @click="setTable">Create seats</button>
+
+    </div>
   </div>
 
   <div class="table-list" v-if="tableIsSet">
     <ul>
-      <li v-for="(n, index) in tableCount" :key="index" @dblclick="booking">
-        0/{{ seats[n-1] }}
+      <li v-for="(seat, index) in seats" :key="index" @dblclick="openModal(index)">
+        <template v-if="!this.bookingDetails[index]">
+          <p>0 / {{ seat }}</p>
+        </template>
+        <template v-if="this.bookingDetails[index]">
+          <p>{{ this.bookingDetails[index].seats }} / {{ seat }}</p>
+          <p>{{ this.bookingDetails[index].name }} 先生/小姐</p>
+          <p>{{ this.bookingDetails[index].time }}</p>
+        </template>
+        
       </li>
     </ul>
+
+    <div v-if="bookingId!=null" class="booking-detail">
+      <form action="#">
+        <label for="number">人數</label>
+        <input type="number" id="number" v-model="bookingDetail.seats">
+        <label for="name">姓名</label>
+        <input type="text" id="name" v-model="bookingDetail.name">
+        <label for="phone">電話</label>
+        <input type="text" id="phone" v-model="bookingDetail.phone">
+        <label for="time">時間</label>
+        <input type="text" id="time" v-model="bookingDetail.time">
+      </form>
+      <button @click="bookingDone(index)">Booking</button>
+      <button class="close" @click="cancelBooking()">X</button>
+    </div>
   </div>
 
-  <div class="booking-detail">
-    <form action="#">
-      <label for="number">人數</label>
-      <input type="number" id="number">
-      <label for="name">姓名</label>
-      <input type="text" id="name">
-      <label for="phone">電話</label>
-      <input type="text" id="phone">
-      <label for="time">時間</label>
-      <input type="text" id="time">
-    </form>
-    <button @click="bookingDone">Booking</button>
-  </div>
+  
 </template>
 
 <script>
@@ -48,21 +62,48 @@ export default {
   },
   data() {
     return {
-      tableCount: 0,
+      tableCount: null,
       tableIsSet: false,
+      showDetail: false,
       seats: [],
+      bookingId: null,
+      bookingDetail: {
+        seats: null,
+        name: null,
+        phone: null,
+        time: null,
+      },
+
+      bookingDetails: [],
     };
   },
   methods: {
     setTable(){
       this.tableIsSet = true;
     },
-    booking(){
-
+    openModal(index){
+      this.bookingId = index;
+      if (!this.bookingDetails[index]) {
+        this.bookingDetails = {
+          seats: null,
+          name: null,
+          phone: null,
+          time: null,
+        };
+      } else {
+        this.bookingDetail = Object.assign({}, this.bookingDetails[index]);
+      }
     },
     bookingDone(){
-      
+      this.bookingDetails[this.bookingId] = Object.assign({}, this.bookingDetail);
+      this.bookingId = null;
+      this.bookingDetail =  [];
+    },
+    cancelBooking(){
+      this.bookingId = null;
+      this.bookingDetail =  [];
     }
+    
   },
 };
 </script>
@@ -104,6 +145,8 @@ h1 {
   ul {
     display: flex;
     li {
+      display: flex;
+      flex-direction: column;
       padding: 30px;
       margin: 0 10px;
       border: 1px solid #000;
@@ -114,6 +157,7 @@ h1 {
 
 .booking-detail {
   position: absolute;
+  display: block;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
@@ -121,6 +165,7 @@ h1 {
   border: 1px solid #000;
   border-radius: 10px;
   background: #eee;
+
   form {
     display: flex;
     flex-direction: column;
@@ -130,6 +175,17 @@ h1 {
   }
   button {
     margin-top: 10px;
+  }
+  .close {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    right: 10px;
+    top: 5px;
+    border: none;
+    border-radius: 50%;
+    font-size: 18px;
+    cursor: pointer;
   }
 }
 </style>
